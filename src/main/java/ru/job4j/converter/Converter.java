418b8e3cb2499613c23bdaa4c7a5749cc74bf2c3
Kollersan <kollersan@gmail.com>
2022-03-19 21:46:53 +0500

    1. Модульные тесты. [#159435]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -8,7 +8,7 @@ public class Converter {
 
     public static float rubleToDollar(float value) {
         float rsl = value / 60;
-        return rsl;
+        return -1;
     }
 
     public static void main(String[] args) {
