784b54d188ddcb224e89f4f5f76d30df9c842515
Kollersan <kollersan@gmail.com>
2022-03-23 20:38:53 +0500

    5.5. Простое число [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    @SuppressWarnings("checkstyle:SimplifyBooleanReturn")
+    public static boolean check(int number) {
+        boolean a = false;
+        for (int i = 2; i <= number - 1; i++) {
+            if (number % i == 0) {
+                a = false;
+                break;
+            }
+            a = true;
+        }
+        return a;
+    }
+}
